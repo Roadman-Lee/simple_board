c4c766cbf1af5551273227a853d5a9a8b83570ab
Roadman <rdmshine@Roadmanui-iMac.local>
2022-05-17 12:41:37 +0900

    create models and db setting

---
+++ board/models/comment.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Comment(models.Model):
+    ref_id = models.ForeignKey(
+        "Board", related_name="board_comment", on_delete=models.CASCADE
+    )
+    ref_comment_id = models.ForeignKey(
+        "Comment", related_name="ref_comment", on_delete=models.CASCADE
+    )
+    comment = models.CharField(blank=True, max_length=255)
+    nickname = models.CharField(max_length=255, null=False)
+    password = models.CharField(max_length=255, null=False)
+    comment_lev = models.PositiveSmallIntegerField(default=0)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
