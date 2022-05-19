ad7320c0058603fbb3f863adb36a840a2d408147
asd <pa6712@naver.com>
2022-05-19 17:23:45 +0900

    dev 수정

--- board/urls.py
+++ board/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from board import views
 
-urlpatterns = [path("", views.board, name="board")]
+urlpatterns = [
+    path("", views.board, name="board"),
+    path('insert/', views.insert, name="board_insert")
+]
---
+++ y_board/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YBoardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'y_board'
