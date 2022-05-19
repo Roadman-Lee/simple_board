c444aa2453495bc6fe54d4b51dc21126a9db8a61
asd <pa6712@naver.com>
2022-05-19 20:18:58 +0900

    dev 수정

--- board/urls.py
+++ board/urls.py
@@ -4,5 +4,6 @@ from board import views
 
 urlpatterns = [
     path("", views.board, name="board"),
-    path('insert/', views.insert, name="board_insert")
+    path('insert/', views.insert, name="board_insert"),
+    path('test-views/', views.TestViews.as_view(), name="test_list")
 ]
