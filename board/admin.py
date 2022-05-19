0e0eabd2f0a5639f9af1b5d9734cf8b58dff73e0
Roadman <rdmshine@Roadmanui-iMac.local>
2022-05-19 17:19:45 +0900

    admin page update

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from board.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'board_id', 'title', 'article', 'password', 'nickname', 'is_deleted']
+    list_display_links = ['pk', 'board_id', 'title', 'article', 'nickname']
+    list_filter = ['board_id', 'nickname', 'is_deleted']
+    search_fields =['board_id', 'title', 'article', 'nickname']
+
+
